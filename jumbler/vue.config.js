feba840d74db9c4089aedcfb49f00874b62c0c82
Aaron Thomas <ajthomas2019@gmail.com>
2024-09-06 22:34:32 -0400

    Updated files for deployment

--- jumbler/vue.config.js
+++ jumbler/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Jumbler/'  // Replace 'your-repository-name' with your actual GitHub repository name
+    : '/'
 })
+
